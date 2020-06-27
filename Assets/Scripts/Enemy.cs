036f684731eaeb20671994a880905e15732a7274
DESKTOP-627PM04\ypb83 <ybarillas@horizon.csueastbay.edu>
2020-06-26 18:27:31 -0700

    a new enemy has appeared

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float chaseDistance;
+    public float stopDistance;
+    public GameObject target;
+
+    private float targetDistance;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        targetDistance = Vector2.Distance(transform.position, target.transform.position);
+        if (targetDistance < chaseDistance && targetDistance > stopDistance)
+            ChasePlayer();
+        else
+            StopChasePlayer();
+    }
+
+    private void StopChasePlayer()
+    {
+        //nothing wow
+    }
+
+    private void ChasePlayer()
+    {
+        if (transform.position.x < target.transform.position.x)
+            GetComponent<SpriteRenderer>().flipX = false;
+        else
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
