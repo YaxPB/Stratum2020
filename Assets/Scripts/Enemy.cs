e67e2b9db6643cc716e11e2fc723639cc546e28c
DESKTOP-627PM04\ypb83 <ybarillas@horizon.csueastbay.edu>
2020-07-01 19:30:45 -0700

    some intial attacks with demo animations

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int maxHealth = 100;
+    int currentHealth;
+
     public float speed;
     public float chaseDistance;
     public float stopDistance;
+
+    public Animator anim;
     public GameObject target;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask playerLayer;
+    public int attackDamage = 20;
 
     private float targetDistance;
 
+    public float attackRate = 1.5f;
+    float nextAttack = 0f;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
     
     void Update()
     {
         targetDistance = Vector2.Distance(transform.position, target.transform.position);
         if (targetDistance < chaseDistance && targetDistance > stopDistance)
-            ChasePlayer();
+        {
+            ChasePlayer(); 
+        }
         else
             StopChasePlayer();
+
+        if (targetDistance <= stopDistance)
+        {
+            if (Time.time >= nextAttack)
+            {
+                    Debug.Log("player next to me");
+                    EnemyAttack();
+                    nextAttack = Time.time + 1f / attackRate;
+            }
+        }
     }
 
     private void StopChasePlayer()
@@ -38,5 +63,60 @@ public class Enemy : MonoBehaviour
             GetComponent<SpriteRenderer>().flipX = true;
 
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //play hurt anim
+        anim.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        anim.SetBool("IsDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("player next to me");
+            EnemyAttack();
+        }
+    }
+
+    public void EnemyAttack()
+    {
+        //play attack anim
+        anim.SetTrigger("EAttack");
+
+        //detect player in range
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayer);
+
+        //apply damage 
+        foreach (Collider2D player in hitPlayer)
+        {
+            player.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    public Animator anim;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    public int attackDamage = 40;
+
+    public float attackRate = 1.5f;
+    float nextAttack = 0f;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextAttack)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Attack();
+                nextAttack = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //play attack anim
+        anim.SetTrigger("Attack");
+
+        //detect enemies in range
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        
+        //apply damage 
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //play hurt anim
+        anim.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        anim.SetBool("IsDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
