4204b55a0762d448c0f5c5378b752e604063e780
DESKTOP-627PM04\ypb83 <ybarillas@horizon.csueastbay.edu>
2020-07-14 15:44:31 -0700

    shitty little update

--- Assets/Scripts/BallBall.cs
+++ Assets/Scripts/BallBall.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BallBall : MonoBehaviour
 {
+    public float thrust;
+    public float knockTime;
+
     public float speed = 20f;
     public int damage = 10;
     public Rigidbody2D rb;
@@ -19,12 +22,33 @@ public class BallBall : MonoBehaviour
     {
         Debug.Log(hitInfo.name);
         Enemy enemy = hitInfo.GetComponent<Enemy>();
+        Rigidbody2D enemyRb = hitInfo.GetComponent<Rigidbody2D>();
         if (enemy != null)
         {
+            //KNOCKBACK CODE
+            enemy.speed = enemy.speed / 2;
+            enemyRb.isKinematic = false;
+            Vector2 difference = enemyRb.transform.position - transform.position;
+            difference = difference.normalized * thrust;
+            enemyRb.AddForce(difference, ForceMode2D.Force);
+            StartCoroutine(KnockCo(enemyRb, enemy));
+
             enemy.TakeDamage(damage);
             Destroy(gameObject);
         }
 
         //Instantiate(impactEffect, transform.position, transform.rotation);
+    }
+
+    private IEnumerator KnockCo(Rigidbody2D enemy, Enemy info)
+    {
+        if(enemy != null)
+        {
+            yield return new WaitForSeconds(knockTime);
+            enemy.velocity = Vector2.zero;
+            //speed the enemy back up after the knockback;
+            info.speed = info.regSpeed;
+            //enemy.isKinematic = true;
+        }
     }
 }
