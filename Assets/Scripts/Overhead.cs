eb8990922e024aa19aa9cecd84b9027e88291fa3
DESKTOP-627PM04\ypb83 <ybarillas@horizon.csueastbay.edu>
2020-07-25 03:08:18 -0700

    Setting up an official tutorial, fixed bugs related to dodge and wave spawning, and started inital overhead enemy health bar

---
+++ Assets/Scripts/Overhead.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Overhead : MonoBehaviour
+{
+    int totalPool;
+    int changedPool;
+
+    public GameObject foos;
+    public Enemy[] maxhealthPool;
+    public Enemy[] globalPool;
+
+    public HealthBar healthBar;
+    public GameObject healthCanvas;
+
+    //GameObject[] Enemies;
+
+    bool counted;
+    bool checkedHealth = false;
+
+    void Start()
+    {
+        maxhealthPool = foos.GetComponentsInChildren<Enemy>();
+        //healthCanvas.SetActive(true);
+    }
+    
+    void Update()
+    {
+        //sets the waves entire health pool
+        if (!counted)
+        {
+            foreach (var health in maxhealthPool)
+            {
+                totalPool += health.currentHealth;
+            }
+        }
+        else
+        {
+            healthBar.SetMaxHealth(totalPool);
+            healthCanvas.SetActive(true);
+        }
+        counted = true;
+
+        globalPool = foos.GetComponentsInChildren<Enemy>();
+
+        AdjustPool();
+        checkedHealth = true;
+    }
+
+    //called to adjust the health pool after each enemy death
+    void AdjustPool()
+    {
+        if (!checkedHealth)
+        {
+            foreach (var healthChange in globalPool)
+            {
+                changedPool += healthChange.currentHealth;
+            }
+        }
+        else
+        {
+            checkedHealth = false;
+            healthBar.SetHealth(changedPool);
+            Debug.Log(changedPool);
+        }
+
+    }
+}
