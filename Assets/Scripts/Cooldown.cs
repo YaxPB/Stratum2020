bebb77f1c4992cf820183e7492177019d17d6ad4
DESKTOP-627PM04\ypb83 <ybarillas@horizon.csueastbay.edu>
2020-08-22 11:24:18 -0700

    So many changes: Reworked tons of small art details with Qianning Touched up on certain UI assets Bg for arena is in, as well as new arches to walk through Fixed Overhead, and turned appearance and disappearance into animations messed up Shay's combat zones in tutorial, fixing them now

--- Assets/Scripts/Cooldown.cs
+++ Assets/Scripts/Cooldown.cs
@@ -7,8 +7,11 @@ public class Cooldown : MonoBehaviour
 {
     public Image berimbauimageCooldown;
     public Image futbolimageCooldown;
+    public Animator berimanim;
+    public Animator futbolanim;
+
     public float berimDown;
-    public float futbolDown = 4f;
+    public float futbolDown;
     public bool berimbauIsCooling;
     public bool ballIsCooling;
     
@@ -17,18 +20,21 @@ public class Cooldown : MonoBehaviour
         /*if(Input.GetKeyDown(KeyCode.Q))
         {
             berimbauIsCooling = true;
-        }*/
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ballIsCooling = true;
-        }
+        }*/ 
 
         if (berimbauIsCooling)
         {
+            //find actual berimbau cooldown time and scale animation
+            berimanim.SetBool("NoteCooling", true);
             berimbauimageCooldown.fillAmount += 1 / berimDown * Time.deltaTime;
             if(berimbauimageCooldown.fillAmount >= 1)
             {
+                berimanim.SetBool("NoteCooling", false);
                 berimbauimageCooldown.fillAmount = 0;
                 berimbauIsCooling = false;
             }
@@ -36,9 +42,12 @@ public class Cooldown : MonoBehaviour
 
         if (ballIsCooling)
         {
+            //find acutual futbol cooldown time and scale 
+            futbolanim.SetBool("BallCooling", true);
             futbolimageCooldown.fillAmount += 1 / futbolDown * Time.deltaTime;
             if (futbolimageCooldown.fillAmount >= 1)
             {
+                futbolanim.SetBool("BallCooling", false);
                 futbolimageCooldown.fillAmount = 0;
                 ballIsCooling = false;
             }
